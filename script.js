b58c6d2bfbc32613244a16bd3d10bcf503e9143d
Shuen Yasui <shuenyasui@gmail.com>
2019-06-11 23:14:31 -0700

    Changed memo output to table instead of ul. Added submit button. Added delete and clear buttons for each entry.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const form = document.querySelector('form');
-const ul = document.querySelector('ul');
 const input = document.getElementById('item');
-const btn_submit = document.getElementById('submit');
-const btn_clear = document.getElementById('clear');
+const btn_del_submit = document.getElementById('submit');
+const btn_del_clear = document.getElementById('clear');
+const tbl = document.querySelector('table');
 
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+const allItems = JSON.parse(localStorage.getItem('items'));
+
+console.log(itemsArray);
+console.log(allItems);
 
 const liMaker = (text) => {
-  const li = document.createElement('li');
-  li.textContent = text;
-  ul.appendChild(li);
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  const btn_del = document.createElement('button');
+  const btn_update = document.createElement('button');
+  tr.appendChild(td);
+  td.appendChild(document.createTextNode(text));
+  btn_del.innerHTML = "delete";
+  btn_update.innerHTML = "update";
+  btn_del.onclick = function(){
+    tbl.removeChild(tr);
+  };
+  btn_update.onclick = function(){
+    console.log(localStorage);
+    let i = localStorage.getItem('items');
+    console.log(i);
+    td.childNodes[0].data = "fasdfsd";
+  }
+  td.appendChild(btn_del);
+  td.appendChild(btn_update);
+  tbl.appendChild(tr);
 }
 
-data.forEach(item => {
+allItems.forEach(item => {
   liMaker(item);
 });
 
@@ -34,14 +54,14 @@ form.addEventListener('submit', function (e) {
   submitNewEntry();
 });
 
-btn_submit.addEventListener('click', function(){
+btn_del_submit.addEventListener('click', function(){
   submitNewEntry();
 });
 
-btn_clear.addEventListener('click', function () {
+btn_del_clear.addEventListener('click', function () {
   localStorage.clear();
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
+  while (tbl.firstChild) {
+    tbl.removeChild(tbl.firstChild);
   }
   itemsArray = [];
 });
